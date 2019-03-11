69ec4414d111713c3f6104ad22e8a3ebba404f19
J. Gabriel Corpas <naoxink@icloud.com>
2019-03-11 07:48:09 +0100

    Update is.lib.js

--- src/is.lib.js
+++ src/is.lib.js
@@ -3,26 +3,48 @@ class Is {
   defined(v){
     return typeof v !== 'undefined'
   }
-  
+
   boolean(v){
     return typeof v === 'boolean'
   }
-  
+
   number(v){
     return typeof v === 'number'
   }
-  
+
+  null(v){
+    return v === null
+  }
+
   array(v){
     return v.constructor === [].constructor
   }
-  
+
   object(v){
     return v.constructor === {}.constructor
   }
-  
+
   property(prop){
-    return (obj) => {
-      return obj.hasOwnProperty(prop)
+    return {
+      of: obj => obj.hasOwnProperty(prop)
+    }
+  }
+
+  greater(variable){
+    return {
+      than: variable2 => variable > variable2
+    }
+  }
+
+  smaller(variable){
+    return {
+      than: variable2 => variable < variable2
+    }
+  }
+
+  equal(variable){
+    return {
+      and: variable2 => variable === variable2
     }
   }
 
